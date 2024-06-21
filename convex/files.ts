d0f09e98d2628a990f3e1a13d8fe06a016959210
jaehyunjang <hyuni2020@gmail.com>
2024-06-21 17:06:23 +0900

    파일업로드 다이얼로그

--- convex/files.ts
+++ convex/files.ts
@@ -2,6 +2,16 @@ import { ConvexError, v } from 'convex/values'
 import { mutation, MutationCtx, query, QueryCtx } from './_generated/server'
 import { getUser } from './users'
 
+export const generateUploadUrl = mutation(async (ctx) => {
+  const identity = await ctx.auth.getUserIdentity()
+
+  if (!identity) {
+    throw new ConvexError('Unauthorized')
+  }
+
+  return await ctx.storage.generateUploadUrl()
+})
+
 export const hasAccessToOrg = async (
   ctx: QueryCtx | MutationCtx,
   tokenIdentifier: string,
@@ -19,6 +29,7 @@ export const createFile = mutation({
   args: {
     name: v.string(),
     orgId: v.string(),
+    fileId: v.id('_storage'),
   },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity()
@@ -41,6 +52,7 @@ export const createFile = mutation({
     await ctx.db.insert('files', {
       name: args.name,
       orgId: args.orgId,
+      fileId: args.fileId,
     })
   },
 })
